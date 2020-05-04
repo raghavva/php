fb043d29b020cc9f99316a7b2e97684f74af9830
raghavva <63421251+raghavva@users.noreply.github.com>
2020-05-04 11:01:52 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,11 @@
+<?php
+$bookid=$_POST['bookid'];
+$bookname=$_POST['bookname'];
+$subject=$_POST['subject'];
+$con=mysqli_connect('localhost','root');
+mysqli_select_db($con,'library');
+$q="insert into bookinfo(bookid,bookname,subject)
+values(bookid,'bookname','subject')";
+$status=mysqli_query($con,$q);
+mysqli_close($con);
+?>
