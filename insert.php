b09aa9bea76bda8e5f9f25ea75e09f9f24bac9f9
raghavva <63421251+raghavva@users.noreply.github.com>
2020-05-05 10:57:32 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@ $subject=$_POST['subject'];
 $con=mysqli_connect('localhost','root');
 mysqli_select_db($con,'library');
 $q="insert into bookinfo(bookid,bookname,subject)
-values(bookid,'bookname','subject')";
+values($bookid,'$bookname','$subject')";
 $status=mysqli_query($con,$q);
 mysqli_close($con);
 ?>
